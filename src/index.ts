5c86ba0d36c442b6eab29abd53b34e787f817293
DimaZako <116648363+DimaZako@users.noreply.github.com>
2025-12-09 13:32:07 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+const userName: string = "дмитро";
+const age: number = 20;
+const isStudent: boolean = true;
 
+function hello(name: string, age: number, student: boolean): string {
+  return `Привіт, ${name}! Тобі ${age} років. Статус: ${student ? "студент" : "не студент"}.`;
+}
+
+console.log(hello(userName, age, isStudent));
